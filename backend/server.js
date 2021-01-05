9b614556967c08aa94300e377badb88c9fc04ff7
truelav <madan.serghei@yahoo.com>
2021-01-04 18:04:00 -0800

    first commit

---
+++ backend/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const uuid = require('uuid/v4');
+
+const app = express();
+
+const DUMMY_PRODUCTS = [
+  {"id":"0c2fd383-9047-4848-a1a1-3b8c5f3929c9","title":"Socks","price":12.90},
+  {"id":"0c2fd383-9047-4848-a1a1-3b8c5f3929c8","title":"Shoes","price":9.90}
+]; // not a database, just some in-memory storage for now
+
+app.use(bodyParser.json());
+
+// CORS Headers => Required for cross-origin/ cross-server communication
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
+
+app.get('/products', (req, res, next) => {
+  res.status(200).json({ products: DUMMY_PRODUCTS });
+});
+
+app.post('/product', (req, res, next) => {
+  const { title, price } = req.body;
+
+  if (!title || title.trim().length === 0 || !price || price <= 0) {
+    return res.status(422).json({
+      message: 'Invalid input, please enter a valid title and price.'
+    });
+  }
+
+  const createdProduct = {
+    id: uuid(),
+    title,
+    price
+  };
+
+  DUMMY_PRODUCTS.push(createdProduct);
+
+  res
+    .status(201)
+    .json({ message: 'Created new product.', product: createdProduct });
+});
+
+app.listen(5000); // start Node + Express server on port 5000
