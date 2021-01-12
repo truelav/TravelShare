1bc12bc759c5e65443700b5c83639b95aa8fe334
truelav <madan.serghei@yahoo.com>
2021-01-12 11:22:44 -0800

    dynamic button component

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -10,6 +10,13 @@ const Users = () => {
       image:
         'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
       places: 3
+    },
+    {
+      id: 'u2',
+      name: 'Vasile Patlagica',
+      image:
+        'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      places: 1
     }
   ];
 
